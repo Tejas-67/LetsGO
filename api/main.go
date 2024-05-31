a106df2a066cbfa0f40eedd1c596a1ea1109d711
Tejas-67 <tejasjha3210@gmail.com>
2024-05-31 20:58:48 +0530

    routes and stuff

--- api/main.go
+++ api/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"log"
 	"math/rand"
 	"net/http"
 	"strconv"
@@ -36,8 +37,48 @@ type Author struct {
 
 func main() {
 	fmt.Println("API ______________________________")
+	router := mux.NewRouter()
+	
+	//seeding 
+	courses = append(courses, Course{
+		CourseId: "1",
+		CourseName: "course1",
+		CoursePrice: 100,
+		Author: &Author{
+			Name: "tejas",
+			Email: "tejas&mm",
+			Website: "tejasdotcom",
+		},
+	})
+	courses = append(courses, Course{
+		CourseId: "2",
+		CourseName: "course2",
+		CoursePrice: 200,
+		Author: &Author{
+			Name: "sohan",
+			Email: "sohan&mm",
+			Website: "sohandotcom",
+		},
+	})
+	courses = append(courses, Course{
+		CourseId: "3",
+		CourseName: "course3",
+		CoursePrice: 300,
+		Author: &Author{
+			Name: "aryan",
+			Email: "aryan&mm",
+			Website: "aryandotcom",
+		},
+	})
+ 
+	router.HandleFunc("/", serverHome).Methods("GET")
+	router.HandleFunc("/courses", getAllCourses).Methods("GET")
+	router.HandleFunc("/course/{id}", getOneCourse).Methods("GET")
+	router.HandleFunc("/course", createOneCourse).Methods("POST")
+	router.HandleFunc("/course/{id}", updateCourse).Methods("PUT")
+	router.HandleFunc("/course/{id}", deleteCourse).Methods("DELETE")
+	log.Fatal(http.ListenAndServe(":3000", router))
 }
-
 //controller
 
 // serve-home route
@@ -69,7 +110,7 @@ func getOneCourse(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode("No course found with given id")
 }
 
-func createOneCourse(r *http.Request, w http.ResponseWriter) {
+func createOneCourse(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Add one course")
 	w.Header().Set("Content-Type", "applicatoin/json")
 
@@ -93,4 +134,44 @@ func createOneCourse(r *http.Request, w http.ResponseWriter) {
 	courses = append(courses, course)
 	json.NewEncoder(w).Encode(course)
 	return
+}
+
+func updateCourse(w http.ResponseWriter, r *http.Request){
+	fmt.Println("updating a course......................")
+	w.Header().Set("Content-Type", "application/json")
+
+	//first -> grab id from req
+	params := mux.Vars(r)
+	
+	//loop, id, remove, add with my ID
+	for index, course := range courses{
+		if course.CourseId == params["id"]{
+			courses = append(courses[:index], courses[index+1:]...)
+			var c Course
+			_ = json.NewDecoder(r.Body).Decode(&c)
+			courses = append(courses, c)
+			c.CourseId = params["id"]
+			courses = append(courses, c)		
+			json.NewEncoder(w).Encode(c)
+			return	
+		}
+	}
+	// course not found -> send a response
+	json.NewEncoder(w).Encode("Course not found!!")
+}
+
+
+func deleteCourse(w http.ResponseWriter, r *http.Request){
+	fmt.Println("Deleting")
+	w.Header().Set("Content-Type", "application/json")
+
+	params := mux.Vars(r)
+	for index, course := range courses{
+		if course.CourseId == params["id"]{
+			courses = append(courses[:index], courses[index+1:]...)
+			json.NewEncoder(w).Encode("Successfully deleted course")
+			return
+		}
+	}
+	json.NewEncoder(w).Encode("Couldn't find course with provided ID")
 }
